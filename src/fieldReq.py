da7d0f550f9917318d42b406f77b69e60cfb2de7
Salaxy <salaxy@web.de>
2015-02-03 23:46:42 +0100

    - initial

---
+++ src/fieldReq.py
@@ -0,0 +1,45 @@
+'''
+Created on 03.02.2015
+
+@author: Salaxy
+'''
+
+'''
+Created on 02.02.2015
+
+@author: Andy Klay
+'''
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    edges = cv2.Canny(frame,100,200)
+    #http://en.wikipedia.org/wiki/Canny_edge_detector
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    #cv2.imshow('frame',frame)
+    #cv2.imshow('mask',mask)
+    #cv2.imshow('res',res)
+    cv2.imshow('edges',edges)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
