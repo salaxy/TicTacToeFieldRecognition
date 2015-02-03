d4c707be650d6bdeda5684429bbde84d4d3435ca
Salaxy <salaxy@web.de>
2015-02-04 00:54:36 +0100

    - find contours

--- src/fieldReq.py
+++ src/fieldReq.py
@@ -23,6 +23,12 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     edges = cv2.Canny(frame,100,200)
     #http://en.wikipedia.org/wiki/Canny_edge_detector
+    
+    contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+    
+    #cnt = contours[4]
+    #cv2.drawContours(frame, [cnt], 0, (0,255,0), 3)
 
     # define range of blue color in HSV
     lower_blue = np.array([110,50,50])
@@ -34,10 +40,11 @@ while(1):
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    #cv2.imshow('frame',frame)
+    cv2.imshow('frame',frame)
     #cv2.imshow('mask',mask)
     #cv2.imshow('res',res)
-    cv2.imshow('edges',edges)
+    #cv2.imshow('edges',edges)
+    
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
