7ef5d28516c5f7320004194a9a2f9e9317960df4
Salaxy <salaxy@web.de>
2015-02-04 13:56:30 +0100

    - skip small contours

--- src/fieldReq.py
+++ src/fieldReq.py
@@ -24,23 +24,38 @@ while(1):
     edges = cv2.Canny(frame,100,200)
     #http://en.wikipedia.org/wiki/Canny_edge_detector
     
-    contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #contours, hierarchy = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours, -1, (0,255,0), 3)
     
     #cnt = contours[4]
     #cv2.drawContours(frame, [cnt], 0, (0,255,0), 3)
     
     for elem in contours:
-        print elem
+        #print elem
         
         #berechne Flaeche des aktuellen Objektes
         #realArea = ABS(cvContourArea(c));
+        realArea = abs(cv2.contourArea(elem))
+        convex = cv2.isContourConvex(elem)
+        print realArea
         
+        #skip small contours
+        if( (realArea < 200) or (not convex)):
+            continue
+        
+        
+        #if (cv2.isContourConvex(elem)):
+        
+            #cv2.drawContours(frame, elem, -1, (0,0,255), 3)
         
         #if (realArea > minAreaBlue && realArea< maxAreaBlue)
+        if ((realArea > 100) and (realArea< 1000)):
         
             #//berechne Umfang des aktuellen Objektes
             #arcLength = cvArcLength(c);
+            #arcLength = cv2.arcLength(curve, closed)
         
             #//berechne Formfaktor des aktuellen Objektes
             #formFactor = ABS(1/ ((arcLength*arcLength) / (4*PI*realArea )));
@@ -55,6 +70,7 @@ while(1):
                 #blueRectObjects.push_back(rect);
         
         
+            cv2.drawContours(frame, elem, -1, (255,0,0), 3)
 
     # define range of blue color in HSV
     #lower_blue = np.array([110,50,50])
