e3e533b7f35b6381ff6581f9f82b110705fc8fe7
Salaxy <salaxy@web.de>
2015-02-05 14:13:02 +0100

    - preparation for rotation

--- src/fieldReq2.py
+++ src/fieldReq2.py
@@ -17,7 +17,8 @@ while(1):
     # Take each frame
     # three frames per secound are enough
     # for gamestate recoqnition
-    time.sleep(0.33)
+    #time.sleep(0.33)
+    time.sleep(0.5)
     _, frame = cap.read()
 
     # Convert BGR to HSV
@@ -64,6 +65,7 @@ while(1):
                 cv2.drawContours(frame, [cnt], -1, (0,255,255), 3)
                 
                 if(formFactor>0.55 and formFactor<0.7):
+                    
                     cv2.drawContours(frame, [cnt], -1, (0,255,0), 3)
 
                     #TODO we need the corners for the robot actions
@@ -99,28 +101,42 @@ while(1):
 
                             if(formFactor>0.55 and formFactor<0.7):
                         
-                                cv2.drawContours(roiFrame, [found], -1, (255,0,255), 3)                        
+                                epsilon = 0.01*cv2.arcLength(found,True)
+                                approx = cv2.approxPolyDP(found,epsilon,True)
+                        
+                                #cv2.drawContours(roiFrame, [found], -1, (255,0,255), 3)                        
                         
                                 moment = cv2.moments(found)
     
+                                #print len(approx)
+                                #cv2.drawContours(roiFrame, [approx], -1, (255,0,255), 3)     
+                                if(len(approx)==4):
+                                    cv2.drawContours(roiFrame, [approx], -1, (255,0,255), 3)       
                         
-                                #rotating the roi for analysis of the sub sections
-                                (h, w) = roi.shape[:2]
-                                cx = int(moment['m10']/moment['m00'])
-                                cy = int(moment['m01']/moment['m00'])
-                                center = (cx, cy)
-                                #center = (w / 2, h / 2)
-                                matrix = cv2.getRotationMatrix2D(center, 10, 1.0)
-                                rotated = cv2.warpAffine(roi, matrix, (w, h))
-                                cv2.imshow("rotated", rotated)
-                    
-                                #prepare correct transform.
-                                
-                                pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-                                pts2 = np.float32([[0,0],[w,0],[0,h],[h,w]])
-                    
-                                #transformMatrix = cv2.getPerspectiveTransform(pts1,pts2)
-                                #dst = cv2.warpPerspective(roi,M,(300,300))
+                                    #rotating the roi for analysis of the sub sections
+                                    (h, w) = roi.shape[:2]
+                                    cx = int(moment['m10']/moment['m00'])
+                                    cy = int(moment['m01']/moment['m00'])
+                                    center = (cx, cy)
+                                    #center = (w / 2, h / 2)
+                                    matrix = cv2.getRotationMatrix2D(center, 10, 1.0)
+                                    rotated = cv2.warpAffine(roi, matrix, (w, h))
+                                    cv2.imshow("rotated", rotated)
+                        
+                                    print approx[0]
+                                    print approx[1]
+                                    print approx[2]
+                                    print approx[3]
+                        
+                                    #prepare correct transform.
+                                    #pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+                                    pts1 = np.float32(approx)
+                                    pts2 = np.float32([[0,0],[h,0],[0,w],[w,h]])
+                        
+                                    transformMatrix = cv2.getPerspectiveTransform(pts1,pts2)
+                                    dst = cv2.warpPerspective(roiFrame,transformMatrix,(w,h))
+                                    #dst = cv2.warpAffine(roiFrame, transformMatrix, (w, h))
+                                    cv2.imshow("dst", dst)
 
     cv2.imshow('frame',frame)
     
