9f7faba1f5b56a9906397695678aa75b7a14745a
Salaxy <salaxy@web.de>
2015-02-05 16:40:27 +0100

    - try another tutorial

--- src/fieldReq2.py
+++ src/fieldReq2.py
@@ -9,6 +9,83 @@ import numpy as np
 from cmath import rect, pi
 import time
 
+def rectify(h):
+    h = h.reshape((4,2))
+    hnew = np.zeros((4,2),dtype = np.float32)
+    
+    add = h.sum(1)
+    hnew[0] = h[np.argmin(add)]
+    hnew[2] = h[np.argmax(add)]
+     
+    diff = np.diff(h,axis = 1)
+    hnew[1] = h[np.argmin(diff)]
+    hnew[3] = h[np.argmax(diff)]
+    
+    return hnew
+
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+     
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+     
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+     
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    #rect = order_points(pts)
+    rect = rectify(pts)
+    (tl, tr, br, bl) = rect
+     
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[0] - bl[0]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[0] - tl[0]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+     
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[1] - br[1]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[1] - bl[1]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+     
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+    [0, 0],
+    [maxWidth - 1, 0],
+    [maxWidth - 1, maxHeight - 1],
+    [0, maxHeight - 1]], dtype = "float32")
+     
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+     
+    # return the warped image
+    return warped
 
 cap = cv2.VideoCapture(0)
 
@@ -106,7 +183,7 @@ while(1):
                         
                                 #cv2.drawContours(roiFrame, [found], -1, (255,0,255), 3)                        
                         
-                                moment = cv2.moments(found)
+                                
     
                                 #print len(approx)
                                 #cv2.drawContours(roiFrame, [approx], -1, (255,0,255), 3)     
@@ -114,34 +191,101 @@ while(1):
                                     cv2.drawContours(roiFrame, [approx], -1, (255,0,255), 3)       
                         
                                     #rotating the roi for analysis of the sub sections
-                                    (h, w) = roi.shape[:2]
-                                    cx = int(moment['m10']/moment['m00'])
-                                    cy = int(moment['m01']/moment['m00'])
-                                    center = (cx, cy)
+                                    ( width, height) = roi.shape[:2]
+                                    #moment = cv2.moments(approx)
+                                    #cx = int(moment['m10']/moment['m00'])
+                                    #cy = int(moment['m01']/moment['m00'])
+                                    #center = (cx, cy)
                                     #center = (w / 2, h / 2)
-                                    matrix = cv2.getRotationMatrix2D(center, 10, 1.0)
-                                    rotated = cv2.warpAffine(roi, matrix, (w, h))
-                                    cv2.imshow("rotated", rotated)
+                                    #matrix = cv2.getRotationMatrix2D(center, 10, 1.0)
+                                    #rotated = cv2.warpAffine(roi, matrix, (w, h))
+                                    #cv2.imshow("rotated", rotated)
                         
-                                    print approx[0]
-                                    print approx[1]
-                                    print approx[2]
-                                    print approx[3]
+                                    print approx
+                                    
+                                    cv2.drawContours(roiFrame, [approx[0]], -1, (0,255,0), 10)
+                                    cv2.drawContours(roiFrame, [approx[1]], -1, (0,255,0), 10)
+                                    cv2.drawContours(roiFrame, [approx[2]], -1, (0,255,0), 10)
+                                    cv2.drawContours(roiFrame, [approx[3]], -1, (0,255,0), 10)
+                                    
+                                    
+                                    ratio = roiFrame.shape[0] / 300.0
+                                    
+                                    
+                                    # now that we have our screen contour, we need to determine
+                                    # the top-left, top-right, bottom-right, and bottom-left
+                                    # points so that we can later warp the image -- we'll start
+                                    # by reshaping our contour to be our finals and initializing
+                                    # our output rectangle in top-left, top-right, bottom-right,
+                                    # and bottom-left order
+                                    pts = approx.reshape(4, 2)
+                                    rect = np.zeros((4, 2), dtype = "float32")
+                                     
+                                    # the top-left point has the smallest sum whereas the
+                                    # bottom-right has the largest sum
+                                    s = pts.sum(axis = 1)
+                                    rect[0] = pts[np.argmin(s)]
+                                    rect[2] = pts[np.argmax(s)]
+                                     
+                                    # compute the difference between the points -- the top-right
+                                    # will have the minumum difference and the bottom-left will
+                                    # have the maximum difference
+                                    diff = np.diff(pts, axis = 1)
+                                    rect[1] = pts[np.argmin(diff)]
+                                    rect[3] = pts[np.argmax(diff)]
+                                     
+                                    # multiply the rectangle by the original ratio
+                                    rect *= ratio
+                                    # now that we have our rectangle of points, let's compute
+                                    # the width of our new image
+                                    (tl, tr, br, bl) = rect
+                                    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[0] - bl[0]) ** 2))
+                                    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[0] - tl[0]) ** 2))
+                                    # ...and now for the height of our new image
+                                    heightA = np.sqrt(((tr[1] - br[1]) ** 2) + ((tr[1] - br[1]) ** 2))
+                                    heightB = np.sqrt(((tl[1] - bl[1]) ** 2) + ((tl[1] - bl[1]) ** 2))
+                                    # take the maximum of the width and height values to reach
+                                    # our final dimensions
+                                    maxWidth = max(int(widthA), int(widthB))
+                                    maxHeight = max(int(heightA), int(heightB))
+                                    # construct our destination points which will be used to
+                                    # map the screen to a top-down, "birds eye" view
+                                    dst = np.array([
+                                    [0, 0],
+                                    [maxWidth - 1, 0],
+                                    [maxWidth - 1, maxHeight - 1],
+                                    [0, maxHeight - 1]], dtype = "float32")
+                                    # calculate the perspective transform matrix and warp
+                                    # the perspective to grab the screen
+                                    M = cv2.getPerspectiveTransform(rect, dst)
+                                    warp = cv2.warpPerspective(roiFrame, M, (maxWidth, maxHeight))
+                                    cv2.imshow("warp", warp)
                         
+                                    #approx=rectify(approx)
                                     #prepare correct transform.
                                     #pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-                                    pts1 = np.float32(approx)
-                                    pts2 = np.float32([[0,0],[h,0],[0,w],[w,h]])
+                                    approx = rectify(approx)
+                                    pts1 = np.array(approx, dtype = "float32")
+                                    pts2 = np.float32([[0,0],[height,0],[0,width],[width,height]])
+                                    #pts2 = np.float32([[0,0],[449,0],[0,449],[449,449]])
                         
                                     transformMatrix = cv2.getPerspectiveTransform(pts1,pts2)
-                                    dst = cv2.warpPerspective(roiFrame,transformMatrix,(w,h))
+                                    dst = cv2.warpPerspective(roiFrame,transformMatrix,(height,width))
+                                    #dst = cv2.warpPerspective(roiFrame,transformMatrix,(449,449))
                                     #dst = cv2.warpAffine(roiFrame, transformMatrix, (w, h))
                                     cv2.imshow("dst", dst)
+                                    
+                                    warped = four_point_transform(roiFrame, pts1)
+                                    cv2.imshow("warped", warped)
 
     cv2.imshow('frame',frame)
+    
+
     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 
 cv2.destroyAllWindows()
+
+
